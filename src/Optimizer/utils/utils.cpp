d7b1e5f172bc0ce13dd070542fdbe77cf524bd91
andy <andnpatterson@gmail.com>
2017-08-19 18:36:14 -0400

    generalize shuffler

---
+++ src/Optimizer/utils/utils.cpp
@@ -0,0 +1,16 @@
+#include "utils.hpp"
+#include <vector>
+#include "util/cdash.hpp"
+#include "util/Random/rand.hpp"
+
+namespace GPUCompute {
+namespace Optimizer {
+namespace Util {
+    std::vector<TFNode> shuffleTensors(ComputeEngine &CE, std::vector<TFNode> Tensors) {
+        int seed = Random::uniformInt(0, 1e8);
+        auto shuffled = _::map<TFNode>(Tensors, [&CE, seed](TFNode T) {
+            return CE.RandomShuffle(T, seed);
+        });
+        return shuffled;
+    }
+}}}
